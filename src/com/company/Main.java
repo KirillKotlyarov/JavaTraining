8e092ec3cd05dd5b5a1713937d8442b06c6adf7e
KirillKotlyarov <kotstudio@mail.ru>
2021-12-22 22:50:52 +0300

    v2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 
 import java.io.IOException;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,8 +13,31 @@ public class Main {
         //Pull.HumanAndFrends();
         //Arr.Pull();
         //ArrAndObjects.Pull();
+algch();
 
 
+    }
+    // Задача:
+    //Пропущенное число
+    // * Вам дан массив из n-1 целых чисел, и эти целые числа находятся в диапазоне от 0 до n.
+    // * В массиве нет дубликатов. В массиве отсутствует одно из целых чисел.
+    // * Напишите эффективный код для поиска недостающего целого числа.
+    public static void algch (){
+        int ar[] = new int [] {2,3,1,7,5,4,8,0,9};
+        Arrays.sort(ar );
+        for (int i = 0; i < ar.length; i++){
+            if (ar [i] == i){
+                continue;
+            }else{
+                int x = ar[i] = ar[i] -1; // Массив из 10 строк с 0 до 9 и нет повторяющихся,
+                //значит когда строка дойдёт до отсутствуюего числа, к примеру 6 это 7я иттерация,
+                //выведется 7-1, т.е. 6
+                System.out.println("Недостающее число: " + x );
+                System.out.println(Arrays.toString(ar) );
+                break;
+            }
+
+        }
 
     }
 }
